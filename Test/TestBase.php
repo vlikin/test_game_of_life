939e7f7d115ff1aea4cc09ae7255edeccadd4067
Viktor Likin <viktor.likin@gmail.com>
2015-09-25 10:23:03 +0300

    The Frame ...

---
+++ Test/TestBase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Test;
+
+class TestBase extends \PHPUnit_Framework_TestCase {
+
+    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+
+    public function getPropertyValue(&$object, $propertyName) {
+        $reflection = new \ReflectionClass(get_class($object));
+        $property = $reflection->getProperty($propertyName);
+        $property->setAccessible(true);
+
+        return $property->getValue($object);
+    }
+
+}
---
+++ TestLife.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file Tests the project.
+ */
+
+require_once(dirname(__FILE__) . '/system.php');
+
+
+use Test\TestBase;
+
+
+class MinerTest extends TestBase {
+    public function testThis() {
+        $this->assertTrue(False);
+    }
+}
